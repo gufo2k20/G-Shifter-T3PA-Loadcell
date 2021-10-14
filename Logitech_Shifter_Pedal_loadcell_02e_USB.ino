eef8b2b82cdea96a11187224cf7f799f495a4e6d
gufo2k20 <76706534+gufo2k20@users.noreply.github.com>
2021-10-14 23:27:55 +0200

    Add files via upload

---
+++ Logitech_Shifter_Pedal_loadcell_02e_USB.ino
@@ -0,0 +1,290 @@
+
+//Logitech Driving Force Shifter USB Adapter 
+//By Armandoiglesias 2018
+//Based on Jason Duncan functionreturnfunction Project
+//Video tutorial https://www.youtube.com/watch?v=dLpWEu8kCec
+//Use Arduino Leonardo
+//Install Joystick Library 
+//Attribution-NonCommercial-NoDerivatives 4.0 International
+
+#include <Joystick.h>
+#include <HX711.h>
+
+// creazione costanti pin cella di carico
+const int LOADCELL_DOUT_PIN1 = 3;
+const int LOADCELL_SCK_PIN1 = 5;
+
+long Freno_cella;
+
+// calibrazione range asse freno a cella
+const long ZonainferioreFreno = -2850;
+const long ZonasuperioreFreno = 12500;
+
+// fattore di calibrazione cella
+const long calibration_factor1 = 30;
+
+// Create the Joystick
+// Joystick_ Joystick;
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
+12, 0,                 // pulsanti, hat switch
+false, false, false,   // X, Y, Z
+false, false, false,   // Rx, Ry, Rz
+false, true,           // rudder, throttle
+true, true, false);    // accelerator, brake, steering
+
+HX711 scale1;
+
+// H-shifter mode analog axis thresholds
+#define HS_XAXIS_12        400
+#define HS_XAXIS_56        600
+#define HS_YAXIS_135       800
+#define HS_YAXIS_246       300
+
+// Sequential shifter mode analog axis thresholds
+#define SS_UPSHIFT_BEGIN   670
+#define SS_UPSHIFT_END     600
+#define SS_DOWNSHIFT_BEGIN 430
+#define SS_DOWNSHIFT_END   500
+
+// Digital inputs definitions
+#define DI_REVERSE         1
+#define DI_MODE            3
+ 
+// Shifter state
+#define DOWN_SHIFT         -1
+#define NO_SHIFT           0
+#define UP_SHIFT           1
+
+// Shifter mode
+#define SHIFTER_H_MODE
+int shiftSSmode = 0;
+
+// Shifter state
+int shift=NO_SHIFT;
+
+int b[16];
+
+int gear=0;               // Default value for H shifter is neutral
+int gearSS=0;             // Default value for SEQ shifter is neutral
+
+// Constant that maps the phyical pin to the joystick button.
+// const int pinToButtonMap = 9;
+
+
+void setup() {
+
+  // switch brake pedal "LOADCELL" or "POT"
+  pinMode(7, INPUT_PULLUP);
+  
+  // loadcell HX711 brake pedal
+  scale1.begin(LOADCELL_DOUT_PIN1, LOADCELL_SCK_PIN1);
+  scale1.set_scale(calibration_factor1);
+  
+  // G29 shifter analog inputs configuration 
+  pinMode(A1, INPUT_PULLUP);   // X axis
+  pinMode(A0, INPUT_PULLUP);   // Y axis
+  pinMode(2, INPUT);           // reverse switch
+
+    for(int i=0; i<16; i++) b[i] = 0;
+  b[DI_MODE] =0;
+  
+  // Initialize Joystick Library
+  Joystick.begin();
+  Joystick.setThrottleRange (165,822);                              //CLUTCH
+  Joystick.setAcceleratorRange (160,800);                           //GAS
+
+  // Last state of the button
+  int lastButtonState = 0;
+
+//Serial.begin(9600);
+}
+
+void loop() {
+/*
+  Serial.println(x);
+  Serial.print("X ");
+  Serial.println(y);
+  Serial.print("Y ");
+*/  
+/*
+  Serial.println (Freno_cella);
+  Serial.print("ACCELERATORE ");
+  Serial.print(g);
+  Serial.print("   FRENO   ");
+  Serial.print(Freno_cella);
+  Serial.print("  FRIZIONE   ");
+  Serial.println(z);
+*/
+
+// richiamo controllo pedaliera
+int T3PA_SWITCH_OLD;
+int T3PA_SWITCH = digitalRead(7);
+
+if (T3PA_SWITCH != T3PA_SWITCH_OLD){                                  //Check BRAKE PEDAL switch(LC/POT)
+  if (T3PA_SWITCH == HIGH){
+    Joystick.setBrakeRange (ZonainferioreFreno, ZonasuperioreFreno);  //BRAKE LOAD CELL
+      } else {
+    Joystick.setBrakeRange (190,885);                                 //BRAKE POT
+      }
+    }
+    T3PA_SWITCH_OLD = T3PA_SWITCH;
+
+if (T3PA_SWITCH == HIGH){
+    T3PA_LOADCELL();
+    } else {
+    T3PA_POT();
+    }
+
+// richiamo controllo shifter
+int shiftmode_x=analogRead(1);       // X axis for shifter mode check
+int shiftmode_y=analogRead(0);       // Y axis for shifter mode check
+
+int shiftmode_press = digitalRead(2);
+if( shiftmode_press == 1 && (shiftmode_x>HS_XAXIS_12 && (shiftmode_x<HS_XAXIS_56))) {
+      if
+        (shiftmode_y > HS_YAXIS_135)
+        {
+          shiftSSmode = 1;
+          }
+        else
+        {
+      if
+        (shiftmode_y < HS_YAXIS_246)
+        {
+          shiftSSmode = 0;
+          }
+        }
+  }
+
+//Serial.print(shiftSSmode);
+
+if (shiftSSmode == 0) {
+    SHIFTER_H();
+    } else {
+    SHIFTER_SS();
+    }
+
+//Joystick.sendState();
+  delay(15);
+}
+
+void T3PA_LOADCELL(){
+  int g=analogRead(3);              //gas
+  int z=analogRead(2);              //clutch
+  Freno_cella = scale1.get_units(); //brake (cella di carico)
+
+  Joystick.setThrottle(z);          //CLUTCH
+  Joystick.setAccelerator(g);       //GAS
+  Joystick.setBrake(Freno_cella);   //BRAKE CELLA DI CARICO
+/*
+  Serial.println (Freno_cella);
+  Serial.print("ACCELERATORE ");
+  Serial.print(g);
+  Serial.print("   FRENO   ");
+  Serial.print(Freno_cella);
+  Serial.print("  FRIZIONE   ");
+  Serial.println(z);
+*/
+}
+
+void T3PA_POT(){
+  int g=analogRead(3);              //gas
+  int z=analogRead(2);              //clutch
+  int f=analogRead(10);             //brake (potenziometro)
+  
+  Joystick.setThrottle(z);         //CLUTCH
+  Joystick.setAccelerator(g);      //GAS
+  Joystick.setBrake(f);            //BRAKE POTENZIOMETRO
+}
+
+void SHIFTER_H(){
+  int H_x=analogRead(1);       // X axis
+  int H_y=analogRead(0);       // Y axis
+
+  int _isreverse = digitalRead(2);
+  int _gear_ = 0;
+
+if( _isreverse == 1 && (H_x>HS_XAXIS_56 && (H_y<HS_YAXIS_246))) {
+      _gear_ = 8;
+      b[DI_REVERSE]= 1;
+  }else{ 
+  
+  if(b[DI_MODE]==0)                      // H-shifter mode?
+  {
+    if(H_x<HS_XAXIS_12)                  // Shifter on the left?
+    {
+      if(H_y>HS_YAXIS_135) _gear_=1;       // 1st gear
+      if(H_y<HS_YAXIS_246) _gear_=2;       // 2nd gear
+    }
+    else if(H_x>HS_XAXIS_56)             // Shifter on the right?
+    {
+      if(H_y>HS_YAXIS_135) _gear_=5;       // 5th gear
+      if(H_y<HS_YAXIS_246) _gear_=6;       // 6th gear
+    }
+    else                               // Shifter is in the middle
+    {
+      if(H_y>HS_YAXIS_135) _gear_=3;       // 3rd gear
+      if(H_y<HS_YAXIS_246) _gear_=4;       // 4th gear
+      }
+    }
+  }
+  
+  if(gear!=6) b[DI_REVERSE]=0;         // Reverse gear is allowed only on 6th gear position
+  
+   if (_gear_ != gear ){
+      gear = _gear_;
+      deactivate();
+      Joystick.setButton(gear-1, HIGH);
+   }
+}
+
+void SHIFTER_SS(){
+
+  int SS_x=analogRead(1);       // X axis
+  int SS_y=analogRead(0);       // Y axis
+
+  int _SSisreverse = digitalRead(2);
+  int _gearSS_;
+  unsigned long _timerSS_;
+  unsigned long _delaySS_ = 250;
+  
+if(_SSisreverse != 1 && ((SS_x>HS_XAXIS_12) && (SS_x<HS_XAXIS_56))) {
+      if (SS_y>HS_YAXIS_135) _gearSS_ = 10;       // SS downshift
+      else if (SS_y<HS_YAXIS_246) _gearSS_ = 9;        // SS upshift
+      else if (SS_y<HS_YAXIS_135 && (SS_y>HS_YAXIS_246)) _gearSS_ = 0;
+  }
+  else _gearSS_ = 0;
+  
+/*
+if(b[DI_MODE]==0)                      // H-shifter mode?
+{
+if(SS_x<HS_XAXIS_12 or (SS_x>HS_XAXIS_56)) // Shifter is NOT in the middle
+{
+_gearSS_ = 0;
+}
+else                                       // Shifter is in the middle
+*/
+
+if (_gearSS_ != 0){  
+   if (_gearSS_ != gearSS){
+          gearSS = _gearSS_;
+          _timerSS_ = millis();{
+            if ((millis()-_timerSS_) > _delaySS_)
+            deactivate();
+            Joystick.setButton(gearSS-1, HIGH);
+          }
+        }
+   } else
+     gearSS = _gearSS_;
+     deactivate();
+     //Serial.println(gearSS);
+     
+//Serial.print(_gearSS_);
+//Serial.print(" --- ");
+//Serial.println(gearSS);
+}
+
+void deactivate(){
+  // Depress virtual button for current gear
+  for(int i = 0; i <= 10 ; i++ )  Joystick.setButton(i, LOW);
+}
